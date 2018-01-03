ede639318507a3e0d386423b96684d12b3135556
SKILL573 <a9101021249@gmail.com>
2018-01-03 15:09:03 +0800

    安裝TRACY套件包

---
+++ config/tracy.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'enabled' => env('APP_DEBUG') === true,
+    'showBar' => env('APP_ENV') !== 'production',
+    'accepts'      => [
+        'text/html',
+    ],
+    // appendTo: body | html
+    'appendTo' => 'body',
+    'editor' => 'subl://open?url=file://%file&line=%line',
+    'maxDepth' => 4,
+    'maxLength' => 1000,
+    'scream' => true,
+    'showLocation' => true,
+    'strictMode' => true,
+    'panels' => [
+        'routing' => true,
+        'database' => true,
+        'view' => true,
+        'event' => false,
+        'session' => true,
+        'request' => true,
+        'auth' => true,
+        'html-validator' => true,
+        'terminal' => true,
+    ],
+];
+
