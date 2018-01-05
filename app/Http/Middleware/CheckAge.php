ae7fd0f8e72c050121b09df36ce1b62e43b7d534
SKILL573 <a9101021249@gmail.com>
2018-01-05 14:55:40 +0800

    檢查年齡MIDDLEWARE內容

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request>20)
+        return $next($request);
+        else
+        return "年齡錯誤";
+    }
+}
